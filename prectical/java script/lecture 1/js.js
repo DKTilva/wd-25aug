bb95482695ac8cd0f88b0aec9c5292108b6f9559
DKTilva <113416466+DKTilva@users.noreply.github.com>
2022-11-08 17:09:56 +0530

    Add files via upload

---
+++ prectical/java script/lecture 1/js.js
@@ -0,0 +1,12 @@
+document.write("This is External Java Script" + "<br>")
+
+var a=97;
+let b="dhruv";
+const c="tilva";
+
+
+
+	document.write(a + "<br>");
+	document.write(b + "<br>");
+	document.write(c);
+	
